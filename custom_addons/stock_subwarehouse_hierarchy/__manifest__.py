68d2767e8b823bcc24338be344e79c58659d4b0c
Stephen Sun <steve-s2024@users.noreply.github.com>
2026-08-03 11:27:05 +0800

    Prioritize inventory and add products app menu

--- custom_addons/stock_subwarehouse_hierarchy/__manifest__.py
+++ custom_addons/stock_subwarehouse_hierarchy/__manifest__.py
@@ -23,6 +23,7 @@
         "data/branding_data.xml",
         "data/language_data.xml",
         "views/branding_views.xml",
+        "views/app_menu_views.xml",
         "views/descendant_inventory_total_views.xml",
         "views/product_attribute_apply_views.xml",
         "views/product_international_mapping_views.xml",
